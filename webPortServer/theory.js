c8f6baa5e3988c6d0a287792a0c3c7fc0f5f648e
CoolShivas <shivas2710cool00@gmail.com>
2025-03-13 12:51:37 +0530

    Restart Your Node.js App Without Nodemon

---
+++ webPortServer/theory.js
@@ -0,0 +1,14 @@
+/****
+ * 
+ * * * * Restart Your Node.js App Without Nodemon :-
+ * 
+ * 
+ * There is an inbuilt solution. That no need of installing the nodemon.
+ * Just directly run node --watch then file name which you want to run.
+ * 
+ * 
+ * Whereas, if you install the nodemon then uninstall it. By writting command (i.e., npm uninstall nodemon).
+ * 
+ * 
+ * 
+ */
